df1d034b90b0ba25733ccd59a1d0c2247f89f789
nullkaaryle <pinja.talosela@gmail.com>
2017-12-29 22:12:58 +0200

    Robotti liikkuu

--- src/org/lejos/example/HelloWorld.java
+++ src/org/lejos/example/HelloWorld.java
@@ -4,12 +4,16 @@ import lejos.nxt.*;
 
 /**
  * Example leJOS Project with an ant build file
- *
+ * 
  */
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Pinni!");
-		Button.waitForPress();
+
+		Motor.A.forward();
+		Motor.B.forward();
+
+		Button.ENTER.waitForPressAndRelease();
+
 	}
 }
